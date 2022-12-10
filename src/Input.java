171a72c0c0ca17cf5056e129a2eb11b9d7e86080
User <Rahimatabaev75@mail.ru>
2022-12-10 18:46:08 +0600

    Rahim wdew

---
+++ src/Input.java
@@ -0,0 +1,22 @@
+public class Input {
+    private String key;
+    private String value;
+
+
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
